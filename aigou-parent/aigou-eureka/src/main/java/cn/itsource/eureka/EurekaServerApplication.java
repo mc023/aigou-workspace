1df0985ae6773696b8582c2075c1c08346603049
muchang <mucchang@163.com>
2020-11-10 21:46:11 +0800

    测试提交主分支

--- aigou-parent/aigou-eureka/src/main/java/cn/itsource/eureka/EurekaServerApplication.java
+++ aigou-parent/aigou-eureka/src/main/java/cn/itsource/eureka/EurekaServerApplication.java
@@ -8,6 +8,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+    
+    /**
+     * 功能描述: <br>
+     * 〈〉
+     * @Param: 这是怎么回实？
+     * @Return: 
+     * @Author: mc
+     * @Date: 2020/11/10 21:45
+     */
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class,args);
     }
